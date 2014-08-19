994a20e08e4264dc80147d3195940f1d167c7db3
Antonino Ingargiola <tritemio@gmail.com>
2014-08-19 11:04:54 -0700

    First commit

---
+++ load_fretbursts.py
@@ -0,0 +1,55 @@
+#%%writefile load_fretbursts.py
+"""
+Helper script to load the FRETBursts from a IPython Notebook.
+
+Run this file from a notebook as follows:
+
+    run load_fretbursts
+
+"""
+
+import sys
+import os
+
+# Import some useful functions for the ipyton notebook
+from IPython.display import display, Math, clear_output
+
+# Process the command-line arguments
+no_gui = len(sys.argv) > 1 and sys.argv[1] == '--nogui'
+
+## Find FRETBursts sources folder
+if os.name == 'posix':
+    # Linux or Mac
+    HOME = os.environ['HOME'] + '/'
+elif os.name == 'nt':
+    # Windows
+    HOME = os.environ['HOMEPATH'] + '/'
+else:
+    raise OSError ("Operating system not recognized (%s)." % os.name)
+
+config_file_name = '.fretbursts'
+with open(HOME + config_file_name) as f:
+    FRETBURSTS_DIR = f.read().strip()
+
+## Save current dir as NOTEBOOK_DIR
+if not 'NOTEBOOK_DIR' in globals():
+    NOTEBOOK_DIR = os.path.abspath('.')
+
+## Change to FRETBursts folder and import the software
+os.chdir(FRETBURSTS_DIR)
+
+from fretbursts import *
+from fretbursts.utils import git
+
+git.print_summary('FRETBursts')
+
+os.chdir(NOTEBOOK_DIR)
+
+## Enable inline plots and QT windows.
+# Workaround for open-file dialog in IPython Notebook
+# see https://github.com/ipython/ipython/issues/5798
+ip = get_ipython()
+ip.enable_matplotlib("inline")
+if not no_gui:
+    ip.enable_gui("qt")
+
---
+++ styles/style_small.py
@@ -0,0 +1,90 @@
+#matplotlib.rcParams.find_all('tic')
+
+ax_tick_width = 0.6
+fontsize = 11
+
+params = {
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ## FIGURE
+    ##
+
+    #'figure.figsize': (x_size_in,y_size_in),
+    #'figure.dpi': 200,
+    
+
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ## LINES
+    ##
+   
+    'lines.linewidth': 2,
+    'axes.color_cycle': ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange'],
+    #'lines.markeredgewidth': 0.3
+    #'lines.markersize': 8,
+		
+
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ## Axes, grid, legend
+    ##
+
+    # Legend frame:
+    #patch.facecolor        : blue
+    #patch.edgecolor        : bcbcbc #eeeeee
+    #patch.antialiased      : True
+    'legend.fancybox': True,
+    'legend.loc': 'best',
+    'patch.linewidth': ax_tick_width,    # legend frame thickness
+    
+    'axes.facecolor': 'white',      # axes background color
+    'axes.edgecolor': 'black',      # axes edge color
+    'axes.axisbelow': True,         # grid below plot lines
+    'axes.linewidth': ax_tick_width,
+    'axes.grid': True,
+
+    'grid.linewidth': ax_tick_width,
+    'grid.linestyle': '-',
+    'grid.color': '#bcbcbc',
+    
+    'xtick.direction': 'in',
+    
+    'xtick.major.size': 6,
+    'xtick.major.width': ax_tick_width,
+    'xtick.major.pad': 4,       # distance to major tick label in points
+
+    'xtick.minor.size': 2,
+    'xtick.minor.width': 0.5,
+    'xtick.minor.pad': 4,       # distance to the minor tick label in points
+
+    'ytick.direction': 'in',
+    
+    'ytick.major.size': 6,
+    'ytick.major.width': ax_tick_width,
+    'ytick.major.pad': 4,       # distance to major tick label in points
+
+    'ytick.minor.size': 2,
+    'ytick.minor.width': 0.5,
+    'ytick.minor.pad': 4,       # distance to the minor tick label in points
+    
+    
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ## FONTS
+    ##
+   
+    #'font.family': 'Arial',
+    'font.sans-serif' : ['Arial', 'Liberation Sans'],
+    'font.size' : fontsize,
+    'xtick.labelsize' : fontsize,
+    'ytick.labelsize' : fontsize,
+    'axes.labelsize' : fontsize,
+    'legend.fontsize' : fontsize - 1,
+    #'font.serif': 'FreeSerif',
+    #'font.size': 6,
+    'axes.titlesize': fontsize,
+    #'axes.labelsize': 6,
+    #'ytick.labelsize': 6,  # fontsize for yticks
+    #'xtick.labelsize': 6,  # fontsize for xticks
+    #'legend.fontsize': 6,
+    'text.usetex': False,
+    #'mathtext.mit' : 'cmtt10.ttf',
+
+}
+rcParams.update(params)
