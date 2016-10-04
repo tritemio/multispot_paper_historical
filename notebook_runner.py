57bfa835856af7c53a2c1318b7ecac0a50e1f2b1
Antonino Ingargiola <tritemio@gmail.com>
2016-10-03 22:26:16 -0700

    Update

--- notebook_runner.py
+++ notebook_runner.py
@@ -1,24 +1,9 @@
-"""
-This modules defines two functions to be used inside an IPython Notebook
-in order to execute other notebooks.
-
-These functions uses [runipy](https://pypi.python.org/pypi/runipy/)
-to run a a single notebook or a template notebook several times
-(with different data files as input).
-
-For the template notebooks the result of each execution (including data and
-plots) are saved as a new notebooks. A parameter (`data_id`) is passed to the
-notebook through an environment variable (`NB_DATA_FILE`). The notebook used
-as template (for example [usALEX-5samples](usALEX-5samples.ipynb)) reads the
-environment variable NB_DATA_FILE to decide which file to process.
-"""
-
 import os
-from runipy.notebook_runner import NotebookRunner
-from IPython.nbformat.current import read, write
-from IPython.display import display, FileLink
+import time
 import pandas as pd
-
+from IPython.display import display, FileLink
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 def run_notebook(notebook_name):
     """Runs the notebook `notebook_name` (file name with no extension).
@@ -28,25 +13,33 @@ def run_notebook(notebook_name):
     to the original file name.
 
     It also displays links to the original and executed notebooks.
-    """
+    """   
+    timestamp_cell = "**Executed:** %s\n\n**Duration:** %d seconds."
     nb_name_full  = notebook_name + '.ipynb'
     display(FileLink(nb_name_full))
+    
+    out_path = 'out_notebooks/'
+    out_nb_name = out_path + notebook_name + '-out.ipynb'
+    
+    nb = nbformat.read(nb_name_full, as_version=4)
+    ep = ExecutePreprocessor(timeout = 3600)
 
-    with open(nb_name_full) as f:
-        notebook = read(f, 'json')
-    out_nb_name = notebook_name + '-out.ipynb'
-
-    r = NotebookRunner(notebook)
+    start_time = time.time()
     try:
-        r.run_notebook()
-    except Exception as e:
+        out = ep.preprocess(nb, {'metadata': {'path': './'}})
+    except Exception:
         msg = 'Error executing the notebook "%s".\n\n' % notebook_name
         msg += 'See notebook "%s" for the traceback.' % out_nb_name
-        print msg
-        raise e
+        print(msg)
+        raise
     finally:
-        with open(out_nb_name, 'w') as f:
-            write(r.nb, f, 'json')
+        # Add timestamping cell
+        duration = time.time() - start_time
+        timestamp_cell = timestamp_cell % (time.ctime(start_time), duration)
+        nb['cells'].insert(0, nbformat.v4.new_markdown_cell(timestamp_cell))
+
+        # Write the executed notebook and display link
+        nbformat.write(nb, out_nb_name)
         display(FileLink(out_nb_name))
 
 
@@ -59,34 +52,47 @@ def run_notebook_template(notebook_name, remove_out=True,
     For each sample, the evaluated notebook containing both plots
     and text output is saved in the 'out_notebooks' folder.
     """
+    timestamp_cell_pattern = "**Executed:** %s\n\n**Duration:** %d seconds."
+    ## Compute TXT data results file name (removing a previous copy)
     assert ph_sel in ['all-ph', 'Dex', 'DexDem', 'AexAem', 'AND-gate', None]
     ph_sel_suffix = '' if ph_sel is None else '-%s' % ph_sel
-    data_fname = 'results/' + notebook_name + '%s.txt' % ph_sel_suffix
+    data_fname = 'results/' + notebook_name + '%s.csv' % ph_sel_suffix
     if remove_out and \
        os.path.exists(data_fname):
             os.remove(data_fname)
 
-    nb_name_full  = notebook_name + '.ipynb'
+    nb_name_full = notebook_name + '.ipynb'
     display(FileLink(nb_name_full))
 
+    out_path = 'out_notebooks/'
+    
+    ep = ExecutePreprocessor(timeout = 3600)
     for data_id in data_ids:
-        os.environ['NBPARAM_DATA_ID'] = data_id
-        if ph_sel is not None:
-            os.environ['NBPARAM_PH_SEL'] = ph_sel
-        with open(nb_name_full) as f:
-            notebook = read(f, 'json')
-        out_nb_name = 'out_notebooks/' + notebook_name + \
-                      '-out%s-%s.ipynb' % (ph_sel_suffix, data_id)
+        nb = nbformat.read(nb_name_full, as_version=4)
+        
+        nb['cells'].insert(1, nbformat.v4.new_code_cell('data_id = "%s"' % data_id))
+        nb['cells'].insert(1, nbformat.v4.new_code_cell('ph_sel_name = "%s"' % ph_sel))
 
-        r = NotebookRunner(notebook)
+        out_nb_name = out_path + notebook_name + '-out%s-%s.ipynb' %\
+                      (ph_sel_suffix, data_id)
+    
+        start_time = time.time()
         try:
-            r.run_notebook()
+            out = ep.preprocess(nb, {'metadata': {'path': './'}})
         except:
-            msg = 'Error executing the notebook for sample "%s".\n\n' % data_id
+            msg = 'Error executing the notebook "%s".\n\n' % notebook_name
             msg += 'See notebook "%s" for the traceback.' % out_nb_name
-            raise RuntimeError(msg)
+            print(msg)
+            raise
         finally:
-            with open(out_nb_name, 'w') as f:
-                write(r.nb, f, 'json')
+            # Add timestamping cell
+            duration = time.time() - start_time
+            timestamp_cell = timestamp_cell_pattern % (time.ctime(start_time), duration)
+            nb['cells'].insert(0, nbformat.v4.new_markdown_cell(timestamp_cell))
+            
+            # Write the executed notebook and display link
+            nbformat.write(nb, out_nb_name)
             display(FileLink(out_nb_name))
-    display(pd.read_csv(data_fname, sep="\s+").set_index('sample'))
+    
+    display(pd.read_csv(data_fname).set_index('sample').round(4))
+    
